5d628be34c0d6340fc2b78f41d1d5948f49673e7
crazyyy <crazyyy@gmail.com>
2015-04-23 13:42:07 +0300

    gallery on page

--- wordpress/wp-content/themes/wp-mypony/front-page.php
+++ wordpress/wp-content/themes/wp-mypony/front-page.php
@@ -5,15 +5,19 @@
             <h2 class="heading heading-pink heading-pony-pink"><span></span>Новые игры <a href="<?php echo home_url(); ?>/pony-game">Все игры ›</a></h2>
             <ul class="new-games-list-wad clearfix">
                 <li><?php the_field( 'adv-game', 'option'); ?></li>
-                <?php
-                  $temp = $wp_query;
-                  $wp_query= null;
-                  query_posts('post_type=pony-game'.'&showposts=3');
-                  while (have_posts()) : the_post();
-                ?>
-                  <?php get_template_part( 'loop-similar-games'); ?>
-                <?php endwhile; ?>
-                <?php $wp_query = null; $wp_query = $temp;?>
+
+              <?php
+                $args = array(
+                  'post_type' => 'pony-game',
+                  'posts_per_page' => 3,
+                  );
+                query_posts($args);
+              ?>
+              <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'loop-similar-games'); ?>
+              <?php endwhile; endif; ?>
+              <?php wp_reset_query(); ?>
+
             </ul>
             <a href="<?php echo home_url(); ?>/pony-game" class="butt butt-midle butt-violet">Все игры</a>
             <!-- article -->
@@ -29,30 +33,40 @@
             <h2 class="heading heading-pink heading-pony-pink-v2"><span></span>Новые серии <a href="<?php echo home_url(); ?>/pony-video">Все серии ›</a></h2>
             <ul class="clearfix new-series-video">
               <?php
-                $temp = $wp_query;
-                $wp_query= null;
-                query_posts('post_type=pony-game'.'&showposts=3');
-                while (have_posts()) : the_post();
+                $args = array(
+                  'post_type' => 'pony-video',
+                  'posts_per_page' => 3,
+                  );
+                query_posts($args);
               ?>
+              <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'loop-new-series'); ?>
-              <?php endwhile; ?>
-              <?php $wp_query = null; $wp_query = $temp;?>
+              <?php endwhile; endif; ?>
+              <?php wp_reset_query(); ?>
             </ul><!-- clearfix new-series-video -->
             <a href="<?php echo home_url(); ?>/pony-video" class="butt butt-midle butt-pink">Все серии</a>
             <article>
               <?php the_field('second-content'); ?>
             </article>
+
             <div class="article-gallery">
                 <h5><span>Галерея</span></h5>
                 <ul class="clearfix gallery-list">
-                    <li><a href=""><img src="" alt=""></a></li>
-                    <li><a href=""><img src="" alt=""></a></li>
-                    <li><a href=""><img src="" alt=""></a></li>
-                    <li><a href=""><img src="" alt=""></a></li>
+                  <?php
+                  $images = get_field('gallery');
+                  if( $images ): ?>
+                    <?php foreach( $images as $image ): ?>
+                    <li>
+                      <a href="<?php echo $image['url']; ?>">
+                       <img src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['alt']; ?>" />
+                      </a>
+                    </li>
+                  <?php endforeach; endif; ?>
                 </ul><!-- gallery-list -->
                 <a href="<?php echo home_url(); ?>/pony-video" class="butt butt-midle butt-orange">Все серии</a>
             </div>
             <!-- /.article-gallery -->
+
             <article>
                 <?php the_field('third-content'); ?>
             </article>
